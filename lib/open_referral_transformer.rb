7e8187dbd2995ab64369d9a4a69359c2e5d74a8c
Shelby Switzer <shelbyswitzer@gmail.com>
2019-06-09 14:11:30 +0300

    add contacts and do some cleanup

--- lib/open_referral_transformer.rb
+++ lib/open_referral_transformer.rb
@@ -15,40 +15,46 @@ class OpenReferralTransformer
   SCHEDULE_HEADERS = %w(id service_id location_id service_at_location_id weekday opens_at closes_at)
   SAL_HEADERS = %w(id service_id location_id description)
   ELIGIBILITIES_HEADERS = %w(id service_id eligibility)
+  CONTACTS_HEADERS = %w(id organization_id service_id service_at_location_id name title department email)
 
   STATE_ABBREVIATIONS = %w(AK AL AR AZ CA CO CT DC DE FL GA HI IA ID IL IN KS KY LA MA MD ME MI MN MO MS MT NC ND NE NH NJ NM NV NY OH OK OR PA RI SC SD TN TX UT VA VT WA WI WV WY)
   DEFAULT_OUTPUT_DIR = "#{ENV["ROOT_PATH"]}/tmp"
+  DEFAULT_INPUT_DIR = "#{ENV["ROOT_PATH"]}/"
 
 
-  attr_reader :organizations_path, :output_organizations_path, :locations_path, :output_locations_path,
-              :services_path, :output_services_path, :mapping, :output_phones_path, :output_addresses_path,
-              :output_schedules_path, :output_sal_path, :output_eligibilities_path, :valid
-
-  attr_accessor :phone_data, :address_data, :schedule_data, :sal_data, :eligibilities_data
+  attr_reader :output_organizations_path, :output_locations_path, :output_services_path,
+              :mapping, :output_phones_path, :output_addresses_path, :output_schedules_path,
+              :output_sal_path, :output_eligibilities_path, :output_contacts_path,
+              :valid, :input_dir, :output_dir,
+              :include_custom, :generate_pkeys
 
   def self.run(args)
     new(args).transform
   end
 
-
-  # Should validate that incoming data is valid-ish, like unique IDs
-
+  # TODO validate that incoming data is valid-ish, like unique IDs
   def initialize(args)
     @mapping = parse_mapping(args[:mapping])
-    @organizations_path = args[:organizations]
-    @locations_path = args[:locations]
-    @services_path = args[:services]
 
+    @input_dir = args[:input_dir] || DEFAULT_INPUT_DIR
     @output_dir = args[:output_dir] || DEFAULT_OUTPUT_DIR
 
+    # "include_custom" indicates that the final output CSVs should include the non-HSDS columns that the original input CSVs had
+    @include_custom = args[:include_custom]
+
+    # "generate_pkeys" indicates that primary keys for new rows should be generated in this script
+    @generate_pkeys
+
+    # TODO DRY this up
     @output_organizations_path = @output_dir + "/organizations.csv"
     @output_locations_path = @output_dir + "/locations.csv"
     @output_services_path = @output_dir + "/services.csv"
     @output_phones_path = @output_dir + "/phones.csv"
     @output_addresses_path = @output_dir + "/addresses.csv"
     @output_schedules_path = @output_dir + "/schedules.csv"
-    @output_sal_path = @output_dir + "/service_at_location.csv"
-    @output_eligibilities_path = @output_dir + '/eligibilities.csv'
+    @output_sal_path = @output_dir + "/service_at_locations.csv"
+    @output_eligibilities_path = @output_dir + "/eligibilities.csv"
+    @output_contacts_path = @output_dir + "/contacts.csv"
 
     @valid = true
 
@@ -60,56 +66,58 @@ class OpenReferralTransformer
     @organizations = []
     @locations = []
     @services = []
+    @contacts = []
   end
 
   def transform
-    # TODO take each toplevel array item from yaml file and convert those
-    transform_each("organizations", organizations_path) if organizations_path
-    # transform_each("locations", locations_path, output_locations_path, LOCATION_HEADERS) if locations_path
-    # transform_each("services", services_path, output_services_path, SERVICE_HEADERS) if services_path
+    mapping.each do |input_file_name, file_mapping|
+      transform_each(input_file_name, file_mapping)
+    end
 
     # write_collected_nested_structures
 
-    write_csv(output_organizations_path, ORGANIZATION_HEADERS, @organizations)
-    write_csv(output_services_path, SERVICE_HEADERS, @services)
-    write_csv(output_locations_path, LOCATION_HEADERS, @locations)
-    write_csv(output_phones_path, PHONE_HEADERS, @phones)
-    write_csv(output_addresses_path, ADDRESS_HEADERS, @addresses)
-    write_csv(output_schedules_path, SCHEDULE_HEADERS, @schedules)
-    write_csv(output_sal_path, SAL_HEADERS, @service_at_locations)
-    write_csv(output_eligibilities_path, ELIGIBILITIES_HEADERS, @eligibilities)
+    write_output_files
 
     # validate_output
 
     return self
   end
 
-  def transform_each(input_csv, path)
-    org_mapping = mapping[input_csv]
+  def transform_each(input_file_name, file_mapping)
+    path = @input_dir + input_file_name
+    org_mapping = file_mapping["columns"]
 
 
+    # Now we want to process each row in a way that allows the row to create multiple objects,
+    # including multiple objects from the same rows.
     CSV.foreach(path, headers: true) do |input|
-      # Now we want to process each row in a way that allows the row to create multiple objects, including multiple objects from the same rows
-      # binding.pry
       collected_data = {}
 
+      # k is the input field_name
+      # org_mapping[k] gives us the array of output fields
       input.each do |k,v|
-        # k is the input field_name
-        # org_mapping[k] gives us the array of output fields
         # turn this into array to be backwards compatible
         output_fields = org_mapping[k].is_a?(Array) ? org_mapping[k] : [org_mapping[k]]
-
 
         # now lets collect each object
         output_fields.compact.each do |output_field|
 
           # collected_data[output_field["model"]] should make it such that collected_data = { "organizations" => {} }
           collected_data[output_field["model"]] ||= {}
-          # binding.pry
-          collected_data[output_field["model"]].merge!(output_field["field"] => v)
-        end
 
+          # Append all string fields marked as "append" to single output field
+          if output_field["append"]
+            existing_string_value = collected_data[output_field["model"]][output_field["field"]] || ""
+            existing_string_value += v.to_s if v
+
+            collected_data[output_field["model"]].merge!(output_field["field"] => existing_string_value)
+          else
+            collected_data[output_field["model"]].merge!(output_field["field"] => v)
+          end
+        end
       end
+
+      # binding.pry
       # now lets pop each object into its respective instance variable collection so it can be written to the right file
       @organizations << collected_data["organizations"] if collected_data["organizations"]
       @services << collected_data["services"] if collected_data["services"]
@@ -118,59 +126,49 @@ class OpenReferralTransformer
       @phones << collected_data["phones"] if collected_data["phones"]
       @schedules << collected_data["schedules"] if collected_data["schedules"]
       @service_at_locations << collected_data["service_at_locations"] if collected_data["service_at_locations"]
+      @contacts << collected_data["contacts"] if collected_data["contacts"]
     end
 
-    #   row = {}
-    #   valid = true
-    #   org_mapping.each do |k, v|
-    #     if v["required"] == true
-    #       if input[k].nil?
-    #         valid = false
-    #         break
-    #       end
-    #     end
-    #     if (v["model"] == input_csv)
-    #       key = v["field"]
-    #       if v["append"]
-    #         row[key] = '' unless row[key]
-    #         if input[k]
-    #           row[key] += input[k].to_s
-    #         end
-    #       else
-    #         row[key] = input[k]
-    #       end
-    #     elsif v["model"] == "phones"
-    #       collect_phone_data(phone_key: k, phone_hash: v, input: input)
-    #     elsif v["model"] == "postal_address"
-    #       collect_address_data(address_key: k, address_hash: v, input: input)
-    #     elsif v["model"] == "regular_schedule"
-    #       process_regular_schedule_text(schedule_key: k, schedule_hash: v, input: input)
-    #     elsif v["model"] == "service_at_locations"
-    #       collect_sal_data(sal_key: k, sal_hash: v, input: input)
-    #     elsif v["model"] == "eligibilities"
-    #       collect_eligibilities_data(eli_key: k, eli_hash: v, input: input)
-    #     end
-    #   end
-    #   if valid
-    #     array << row
-    #   end
-    # end
-    #
-    # write_csv(output_path, headers, org_data)
+      # org_mapping.each do |k, v|
+      #   if v["model"] == "phones"
+      #     # collect_phone_data(phone_key: k, phone_hash: v, input: input)
+      #   elsif v["model"] == "postal_address"
+      #     collect_address_data(address_key: k, address_hash: v, input: input)
+      #   elsif v["model"] == "regular_schedule"
+      #     process_regular_schedule_text(schedule_key: k, schedule_hash: v, input: input)
+      #   elsif v["model"] == "service_at_locations"
+      #     collect_sal_data(sal_key: k, sal_hash: v, input: input)
+      #   elsif v["model"] == "eligibilities"
+      #     collect_eligibilities_data(eli_key: k, eli_hash: v, input: input)
+      #   end
+      # end
+
   end
 
   private
 
-  def collect_phone_data(phone_key:, phone_hash:, input:)
-    key = phone_hash["field"]
-    phone_row = {}
-    phone_row[key] = input[phone_key]
-
-    foreign_key = phone_hash["foreign_key_name"]
-    foreign_key_value = phone_hash["foreign_key_value"]
-    phone_row[foreign_key] = input[foreign_key_value]
-    phone_data << phone_row
+  def write_output_files
+    write_csv(output_organizations_path, ORGANIZATION_HEADERS, @organizations)
+    write_csv(output_services_path, SERVICE_HEADERS, @services)
+    write_csv(output_locations_path, LOCATION_HEADERS, @locations)
+    write_csv(output_phones_path, PHONE_HEADERS, @phones)
+    write_csv(output_addresses_path, ADDRESS_HEADERS, @addresses)
+    write_csv(output_schedules_path, SCHEDULE_HEADERS, @schedules)
+    write_csv(output_sal_path, SAL_HEADERS, @service_at_locations)
+    write_csv(output_eligibilities_path, ELIGIBILITIES_HEADERS, @eligibilities)
+    write_csv(output_contacts_path, CONTACTS_HEADERS, @contacts)
   end
+
+  # def collect_phone_data(phone_key:, phone_hash:, input:)
+  #   key = phone_hash["field"]
+  #   phone_row = {}
+  #   phone_row[key] = input[phone_key]
+  #
+  #   foreign_key = phone_hash["foreign_key_name"]
+  #   foreign_key_value = phone_hash["foreign_key_value"]
+  #   phone_row[foreign_key] = input[foreign_key_value]
+  #   phone_data << phone_row
+  # end
 
   def collect_address_data(address_key:, address_hash:, input:)
     key = address_hash["field"]
